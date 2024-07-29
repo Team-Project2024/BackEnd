43ea3afa9595ab39a0d24bf1ccd841a78c42515a
Kim Gwang Je <gwangjeg14@gmail.com>
2024-07-29 19:42:39 +0900

    Feat: "SchoolLocation Cache 적용"

--- src/main/java/Hoseo/GraduationProject/API/SchoolLocation/Service/SchoolLocationService.java
+++ src/main/java/Hoseo/GraduationProject/API/SchoolLocation/Service/SchoolLocationService.java
@@ -4,6 +4,8 @@ import Hoseo.GraduationProject.API.SchoolLocation.DTO.ResponseSchoolLocation;
 import Hoseo.GraduationProject.API.SchoolLocation.Domain.SchoolLocation;
 import Hoseo.GraduationProject.API.SchoolLocation.Repository.SchoolLocationRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -12,13 +14,18 @@ import java.util.List;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class SchoolLocationService {
     private final SchoolLocationRepository schoolLocationRepository;
 
     @Transactional(readOnly = true)
+    @Cacheable(value = "schoolLocationCache", key = "#schoolLocationIdList",
+            cacheManager = "cacheManager")
     public List<ResponseSchoolLocation> getSchoolLocationList(List<Long> schoolLocationIdList){
         List<SchoolLocation> schoolLocationList = schoolLocationRepository.findAllById(schoolLocationIdList);
         List<ResponseSchoolLocation> responseSchoolLocationList = new ArrayList<>();
+
+        log.info("Fetching data from DB Not Redis Cache");
 
         for(SchoolLocation schoolLocation : schoolLocationList){
             ResponseSchoolLocation responseSchoolLocation = new ResponseSchoolLocation();
